7c38f43c87ae77429780dff94a7ff9d2495725d6
Andrey Yermoshin <ermoshin.andrey2@wb.ru>
2024-11-22 22:21:00 +0300

    ROUND AVATARS!!!1!

--- client/src/components/ProfilePicture.tsx
+++ client/src/components/ProfilePicture.tsx
@@ -9,7 +9,7 @@ export interface ProfilePictureProps {
 export function ProfilePicture({ userId, hasAvatar, size }: ProfilePictureProps) {
     return (
         <div style={{ display: "flex", height: "100%", width: "100%", alignItems: "center", justifyContent: "center" }}>
-            {hasAvatar ? <img style={{ alignSelf: "center" }} src={`/api/users/${userId}/file`} width={size || 150} height={size || 150} /> : <Avatar sx={{ width: size || 150, height: size || 150}} />}
+            {hasAvatar ? <Avatar src={`/api/users/${userId}/file`} sx={{ width: size || 150, height: size || 150 }} /> : <Avatar sx={{ width: size || 150, height: size || 150 }} />}
         </div>
     )
 }
